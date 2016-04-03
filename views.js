1c7437484fe1d2a97a30eb237645f4f1387fbfb5
Alex Scott <alex@Alexs-MacBook-Pro.local>
2016-04-03 12:53:18 +0200

    Adding a single button view

--- views.js
+++ views.js
@@ -12,49 +12,61 @@ namespace.views.wizard = Backbone.View.extend({
   initialize: function(){
     this.render();
   },
-  screenTemplate: _.template('<h1 id="section-title">{{sectionTitle}}</h1>{{ title }} <br /> <p id="screen-instructions">{{ description}}</p>'),
+  screenTemplate: _.template('<h1 id="section-title" class="wizard__question">{{sectionTitle}}</h1>{{ title }} <br /> <div class="wizard__tip">{{ description}}</div>'),
   render: function(){
     console.log(this.model.toJSON());
     this.$el.html(this.screenTemplate(this.model.toJSON()));
-    // Buttons render here.
-//    var buttonsView = new namespace.views.ButtonsView({ el: '#buttons', model: this.model });
+    var buttonsView = new namespace.views.ButtonsView({ model: this.model });
+    return this;
   }
 });
 
 
 namespace.views.ButtonsView = Backbone.View.extend({
+  el: ".wizard__buttons",
 
   initialize: function() {
     this.render();
   },
 
+  buttonsTemplate: _.template('<a href="{{id}}" class="">{{title}}</a>'),
+
+  render: function() {
+    var buttons = this.model.get("buttons");
+    var that = this;
+
+     _.each(buttons, function(button) {
+      var view =  new namespace.views.ButtonView({model: button});
+       that.$el.append(view.render().el);
+    });
+    return this;
+  }
+});
+
+
+namespace.views.ButtonView = Backbone.View.extend({
+  tagName: "div",
+
+  className: "changeme",
+
   events: {
-    "click .but": "transitionUp",
-    "click .but": "tranistionDown",
-},
+    "click .wizard__button": "transitionUp",
+    "click .wizard__button": "tranistionDown",
+  },
 
   transitionUp: function() {
     console.log("Transition up");
     event.preventDefault();
+    this.model.set({selected: true});
 },
 
-  tranistionDown: function() {
-    console.log("Transition down");
-    this.model.set({selected: true});
-    console.log("sel: ", this.model.get('selected'));
-    event.preventDefault();
+  buttonTemplate: _.template('<a href="{{id}}" class="wizard__button">{{title}}</a>'),
+  initialize: function() {
+    this.render();
   },
 
   render: function() {
-    // Move loop into parent function.
-    var buttons = this.model.get("buttons");
-    var b = '';
-     _.each(buttons, function(button) {
-       var compiled = _.template('<a href="" class="but button"><%= title %></a>');
-       b = b + compiled({title: button.buttonTitle});
-    });
-    console.log(this);
-    $(this.el).html(b);
+    this.$el.html(this.buttonTemplate({title: this.model.buttonTitle, id: this.model.id}));
+    return this;
   }
 });
-
