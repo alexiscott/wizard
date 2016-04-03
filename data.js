5a846650946350dc8889b17002710eb3e35cdc78
Alex Scott <alex@Alexs-MacBook-Pro.local>
2016-04-03 10:03:44 +0200

    backbone it

--- data.js
+++ data.js
@@ -21,41 +21,72 @@ var bp_wizard_sections = {
 var bp_wizard_data = 
   [
     {
-    screenId: 0, 
-    screenTitle: "Title of Screen",
-    screenDescription: "Screen description",
-    section: 123,
-    sectionTitle: "SEC 1",
+    screenId: 1, 
+    screenTitle: "Are you sure you want to go through with this?",
+    screenDescription: "Introductory screen to get started",
+    section: 1,
+    sectionTitle: "Intro Section",
     buttons: [
       {
         buttonTitle: "Yes",
-        buttonResult: "Bad choice",
-        buttonLinkTo: 0
+        buttonResult: "You decided to go through with this",
+        buttonLinkTo: 2
       },
       {
         buttonTitle: "No",
-        buttonResult: "Better choice",
-        buttonLinkTo: 1
+        buttonResult: "We regret that this was not for you.",
+        buttonLinkTo: 3
       },
     ]
   },
     {
-    screenId: 1, 
-    screenTitle: "2 Title of Screen",
-    screenDescription: "2 Screen description",
-    section: 123,
-    sectionTitle: "SEC 2",
+    screenId: 2, 
+    screenTitle: "Do you live in the LA area?",
+    screenDescription: "Getting a feel for where you are at",
+    section: 1,
+    sectionTitle: "Intro Section",
     buttons: [
       {
         buttonTitle: "Yes",
-        buttonResult: "Bad choice",
-        buttonLinkTo: 0
+        buttonResult: "Good to have another LA'er on board.",
+        buttonLinkTo: 4
       },
       {
         buttonTitle: "No",
-        buttonResult: "Better choice",
-        buttonLinkTo: 1
+        buttonResult: "We only can help people living in the LA area.",
+        buttonLinkTo: 3
       },
     ]
-  }
+  },
+
+    {
+    screenId: 3, 
+    screenTitle: "Sorry we can't help you",
+    screenDescription: "Try something else instead",
+    section: 1,
+    sectionTitle: "SEC 1",
+    buttons: []
+  },
+
+    {
+    screenId: 4, 
+    screenTitle: "What color pyjamas do you wear?",
+    screenDescription: "Or maybe they are chequered",
+    section: 2,
+    sectionTitle: "SEC 2",
+    buttons: [
+      {
+        buttonTitle: "Blue",
+        buttonResult: "Blue pyjama person.",
+        buttonLinkTo: 5
+      },
+      {
+        buttonTitle: "Any other color or chequered",
+        buttonResult: "Uptight chequerer pyjama person.",
+        buttonLinkTo: 6
+      },
+    ]
+  },
+
+
   ];
